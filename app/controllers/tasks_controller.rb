185e874451fa900e9b1d300c268ceb619cac0523
Rowan Douglas <rowan.douglas29@gmail.com>
2020-11-17 20:15:10 -0300

    completed CRUD, does not look nice and have not refactored yet

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to home_path
+  end
+
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:name, :details, :completed)
+  end
+end
+
